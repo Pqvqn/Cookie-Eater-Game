0e245d3f33215e52110cc4ef1fbd2845f59ee037
Pqvqn <pavanarauch@hotmail.com>
2020-11-07 18:16:00 -0500

    reworking autopilot

--- Cookie Eater/src/items/ItemAutopilot.java
+++ Cookie Eater/src/items/ItemAutopilot.java
@@ -8,12 +8,14 @@ public class ItemAutopilot extends Item{
 	private double speedy;
 	private double initx, inity;
 	private Cookie nearest;
+	private double searchRadius;
 	
 	public ItemAutopilot(Board frame) {
 		super(frame);
 		speedy = 5;
+		searchRadius = 150;
 		name = "Autopilot";
-		desc="Automatically aims towards nearest cookie.`Amplify: Speed increases";
+		desc="Automatically aims towards near cookies.`Amplify: Allowed radius increases ";
 	}
 	public void prepare() {	
 	}
@@ -23,15 +25,24 @@ public class ItemAutopilot extends Item{
 	
 	public void execute() {	
 		if(checkCanceled())return;
-		adjustSpeeds();
-		if(!board.cookies.isEmpty())
-			//user.averageVels(initx,inity,true);
-			user.setXVel(initx,true);
-			user.setYVel(inity,true);
+		if(nearest == null) {
+			nearest = board.nearestCookie(user.getX()+user.getXVel(),user.getY()+user.getYVel());
+			if(Level.lineLength(nearest.getX(),nearest.getY(),user.getX(),user.getY())<searchRadius * board.currFloor.getScale()) {
+				adjustSpeeds();
+			}else {
+				nearest = null;
+			}
+		}else {
+			adjustSpeeds();
+			if(!board.cookies.isEmpty())
+				//user.averageVels(initx,inity,true);
+				user.setXVel(initx,true);
+				user.setYVel(inity,true);
+		}
 	}
 	
 	public void adjustSpeeds() { //aims to nearest cookie
-		nearest = board.nearestCookie(user.getX()+user.getXVel(),user.getY()+user.getYVel());
+		
 		if(nearest==null)return;
 		//double rat = ((board.getAdjustedCycle()/15.0)*speedy*board.currFloor.getScale())/Level.lineLength(user.getX(), user.getY(), nearest.getX(), nearest.getY());
 		double rat = ((board.getAdjustedCycle()/15.0)*(Math.sqrt(Math.pow(user.getXVel(true),2)+Math.pow(user.getYVel(true),2))/Level.lineLength(user.getX(), user.getY(), nearest.getX(), nearest.getY())));
@@ -47,10 +58,12 @@ public class ItemAutopilot extends Item{
 	}
 	public void amplify() {
 		super.amplify();
-		speedy+=5;
+		//speedy+=5;
+		searchRadius+=100;
 	}
 	public void deamplify() {
 		super.deamplify();
-		speedy-=5;
+		//speedy-=5;
+		searchRadius-=100;
 	}
 }
