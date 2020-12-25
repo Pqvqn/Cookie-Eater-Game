5f31dd3319dc72b754a32b8308b428c2dd31e93a
Pqvqn <pavanarauch@hotmail.com>
2020-12-24 22:29:11 -0500

    began writing sound handling class

---
+++ Cookie Eater/src/ce3/Audio.java
@@ -0,0 +1,17 @@
+package ce3;
+
+import java.util.*;
+
+import javax.sound.sampled.*;
+
+public class Audio {
+	
+	private Board board;
+	ArrayList<Clip> clips;
+	
+	public Audio(Board frame) {
+		board = frame;
+		clips = new ArrayList<Clip>();
+	}
+
+}
