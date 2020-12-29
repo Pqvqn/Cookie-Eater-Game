553617907fa3757fc93f1e320646ac500f84fcc1
Pqvqn <pavanarauch@hotmail.com>
2020-12-28 22:02:28 -0500

    improving audio class

--- Cookie Eater/src/ce3/Audio.java
+++ Cookie Eater/src/ce3/Audio.java
@@ -9,31 +9,40 @@ public class Audio {
 	
 	private Board board;
 	public static final float VOLUME_LOW = -15.0f,VOLUME_HIGH = 6.0f,VOLUME_NORM = 0f,VOLUME_RANGE=VOLUME_HIGH-VOLUME_LOW;
+	public HashMap<String,File> loaded; //already loaded sound files to be accessed by string name
+	private final String[] preload = {"bonk2","chomp"}; //files to load at start of program
+	public boolean mute; //whether audio should play at all
 	
 	public Audio(Board frame) {
 		board = frame;
-        
+		mute = false;
+		loaded = new HashMap<String,File>();
+        for(String p : preload) {
+        	File f = new File("Cookie Eater/src/resources/sounds/"+p+".wav");
+			loaded.put(p,f);
+        }
        //playSound("testmusic",VOLUME_HIGH);
 	}
 	
 	//plays sound from audio file
 	public void playFile(File soundfile, float volume) {
+		if(mute)return;
 		try {
-	    	AudioInputStream stream = AudioSystem.getAudioInputStream(soundfile.getAbsoluteFile());  
-	        Clip clip = AudioSystem.getClip();
+			AudioInputStream stream = AudioSystem.getAudioInputStream(soundfile.getAbsoluteFile());
+			Clip clip = AudioSystem.getClip();
 			clip.open(stream);
 	        if(clip.isRunning())clip.stop();
 	        clip.setFramePosition(0);
 	        FloatControl gain = (FloatControl)clip.getControl(FloatControl.Type.MASTER_GAIN);
 	        gain.setValue(volume);
 			clip.start();
-		} catch (LineUnavailableException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (UnsupportedAudioFileException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (LineUnavailableException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}  
@@ -41,10 +50,16 @@ public class Audio {
 	
 	//plays sound based on name of file from sound package
 	public void playSound(String title, float volume) {
-		playFile(new File("Cookie Eater/src/resources/sounds/"+title+".wav"),volume);
+		if(loaded.get(title)!=null) { //if title is already loaded, use the already loaded input stream
+			playFile(loaded.get(title),volume);
+		}else {
+			playFile(new File("Cookie Eater/src/resources/sounds/"+title+".wav"),volume);
+		}
 	}
 	public void playSound(String title) {
 		playSound(title,VOLUME_NORM);
 	}
+	
+	public void setMute(boolean m) {mute = m;}
 	
 }
