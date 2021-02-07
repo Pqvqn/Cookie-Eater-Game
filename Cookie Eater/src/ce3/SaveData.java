8e02f9b411580b22d094b09a0d8eaf060bfaeac7
Pqvqn <pavanarauch@hotmail.com>
2021-02-06 19:23:09 -0500

    building SaveData class

--- Cookie Eater/src/ce3/SaveData.java
+++ Cookie Eater/src/ce3/SaveData.java
@@ -1,5 +1,37 @@
 package ce3;
 
-public class SaveData {
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
 
+public class SaveData {
+	
+	private HashMap<String,Object> dataStorage; //keeps tags and data
+	
+	public SaveData(File f) throws IOException {
+		String data = Files.readString(f.toPath()); //read full file into string
+		interpretString(data);
+	}
+	
+	public SaveData(String s) {
+		interpretString(s);
+	}
+	
+	private void interpretString(String s) {
+		//split sections of data
+		String[] sections = s.split("|");
+		for(int i=0; i<sections.length; i++) {
+			//split tag from information
+			String[] parts = sections[i].split(":");
+			//split parts of information
+			String[] info = parts[1].split(";");
+			//add to data storage
+			dataStorage.put(parts[0],info);
+		}
+	}
+
+	public Object getData(String tag) {
+		return dataStorage.get(tag);
+	}
+	
 }
