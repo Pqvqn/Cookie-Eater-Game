4bf93351a4031495683c898bf9bafec1562768d6
Pqvqn <pavanarauch@hotmail.com>
2021-02-07 20:07:15 -0500

    SaveData reading/writing progress

--- Cookie Eater/src/ce3/SaveData.java
+++ Cookie Eater/src/ce3/SaveData.java
@@ -6,7 +6,7 @@ import java.util.*;
 
 public class SaveData {
 	
-	private HashMap<String,Object> dataStorage; //keeps tags and data
+	private HashMap<String,ArrayList<Object>> dataStorage; //keeps tags and data
 	
 	public SaveData(File f) throws IOException {
 		String data = Files.readString(f.toPath()); //read full file into string
@@ -25,13 +25,38 @@ public class SaveData {
 			String[] parts = sections[i].split(":");
 			//split parts of information
 			String[] info = parts[1].split(";");
+			ArrayList<Object> info2 = new ArrayList<Object>();
+			
+			//test for type of info before adding
+			for(String s2:info) {
+				if(s2.substring(0,1).equals("*")) { //savedata
+					info2.add(new SaveData(s2.substring(1)));
+				}else { //string
+					info2.add(s2);
+				}
+			}
+			
 			//add to data storage
-			dataStorage.put(parts[0],info);
+			dataStorage.put(parts[0],info2);
 		}
 	}
 
 	public Object getData(String tag) {
 		return dataStorage.get(tag);
+	}
+	
+	
+	public String toString() {
+		String ret = "*";
+		Iterator<String> it = dataStorage.keySet().iterator();
+		while(it.hasNext()) {
+			String tag = it.next();
+			ret+="|"+tag+":";
+			for(Object o : dataStorage.get(tag)) {
+				ret+=o.toString()+";";
+			}
+		}
+		return ret;
 	}
 	
 }
