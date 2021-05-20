4d28d368c4d85da90b051c1244a32f0d30bd90ec
Pqvqn <pavanarauch@hotmail.com>
2021-05-19 22:48:45 -0400

    fixed cookieitem color palettes

--- Cookie Eater/src/sprites/SpriteCookie.java
+++ Cookie Eater/src/sprites/SpriteCookie.java
@@ -13,6 +13,7 @@ public class SpriteCookie extends Sprite{
 
 	private Cookie user;
 	private Image base, chip;
+	private BufferedImage finimg;
 	private double scale;
 	private File defBase, splBase, defChip, splChip;
 	private int state;
@@ -36,12 +37,20 @@ public class SpriteCookie extends Sprite{
 		base = ImageIO.read(defBase);
 		chip = ImageIO.read(defChip);
 		state = REG;
+
+		finimg = new BufferedImage(base.getWidth(null),base.getHeight(null),BufferedImage.TYPE_INT_ARGB);
+		Graphics compiled = finimg.getGraphics();
+		compiled.drawImage(base,0,0,null);
+		compiled.drawImage(chip,0,0,null);
+		
 		if(palette>=0) {
 			readColors(ImageIO.read(new File("Cookie Eater/src/resources/cookies/itempalettes.png")));
-			base = convertPalette((BufferedImage)base,0,palette);
-			chip = convertPalette((BufferedImage)chip,0,palette);
+			//base = convertPalette((BufferedImage)base,0,palette);
+			//chip = convertPalette((BufferedImage)chip,0,palette);
+			finimg = convertPalette((BufferedImage)finimg,0,palette);
 			state = ITEM;
 		}
+		
 		imgs.add(base);
 		imgs.add(chip);
 	}
@@ -49,10 +58,18 @@ public class SpriteCookie extends Sprite{
 		if(user.getDecayed()&&state==REG) {
 			base = ImageIO.read(splBase);
 			chip = ImageIO.read(splChip);
+			finimg = new BufferedImage(base.getWidth(null),base.getHeight(null),BufferedImage.TYPE_INT_ARGB);
+			Graphics compiled = finimg.getGraphics();
+			compiled.drawImage(base,0,0,null);
+			compiled.drawImage(chip,0,0,null);
 			state = SPOILED;
 		}else if(!user.getDecayed()&&state==SPOILED) {
 			base = ImageIO.read(defBase);
 			chip = ImageIO.read(defChip);
+			finimg = new BufferedImage(base.getWidth(null),base.getHeight(null),BufferedImage.TYPE_INT_ARGB);
+			Graphics compiled = finimg.getGraphics();
+			compiled.drawImage(base,0,0,null);
+			compiled.drawImage(chip,0,0,null);
 			state = REG;
 		}
 	}
@@ -67,8 +84,9 @@ public class SpriteCookie extends Sprite{
 		scale = board.currFloor.getScale();
 		x = user.getX();
 		y = user.getY();
-		g.drawImage(base,(int)(.5+x-(base.getWidth(null)/10*scale)), (int)(.5+y-(base.getHeight(null)/10*scale)), (int)(2*(.5+base.getWidth(null)/10*scale)), (int)(2*(.5+base.getHeight(null)/10*scale)), null);
-		g.drawImage(chip,(int)(.5+x-(chip.getWidth(null)/10*scale)), (int)(.5+y-(chip.getHeight(null)/10*scale)), (int)(2*(.5+chip.getWidth(null)/10*scale)), (int)(2*(.5+chip.getHeight(null)/10*scale)), null);
+		//g.drawImage(base,(int)(.5+x-(base.getWidth(null)/10*scale)), (int)(.5+y-(base.getHeight(null)/10*scale)), (int)(2*(.5+base.getWidth(null)/10*scale)), (int)(2*(.5+base.getHeight(null)/10*scale)), null);
+		//g.drawImage(chip,(int)(.5+x-(chip.getWidth(null)/10*scale)), (int)(.5+y-(chip.getHeight(null)/10*scale)), (int)(2*(.5+chip.getWidth(null)/10*scale)), (int)(2*(.5+chip.getHeight(null)/10*scale)), null);
+		g.drawImage(finimg,(int)(.5+x-(finimg.getWidth(null)/10*scale)), (int)(.5+y-(finimg.getHeight(null)/10*scale)), (int)(2*(.5+finimg.getWidth(null)/10*scale)), (int)(2*(.5+finimg.getHeight(null)/10*scale)), null);
 
 	}
 }
