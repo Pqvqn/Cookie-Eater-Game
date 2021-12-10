c0b80bb9a65c65823f86253335a0ce56996bf8e7
Pqvqn <pavanarauch@hotmail.com>
2021-12-09 20:43:45 -0500

    preferred themes for Decorations

--- Cookie Eater/src/mechanisms/Decoration.java
+++ Cookie Eater/src/mechanisms/Decoration.java
@@ -10,10 +10,12 @@ public class Decoration extends Mechanism{
 
 	private SpriteMechanism sprite;
 	private String fileName;
+	private String[] preferredThemes; //affects placement to make closer to theme
 	
-	public Decoration(Game frame, Board gameboard, int xPos, int yPos, String name) {
+	public Decoration(Game frame, Board gameboard, int xPos, int yPos, String name, String[] themes) {
 		super(frame, gameboard, xPos, yPos);
 		fileName = name;
+		preferredThemes = themes;
 		try {
 			sprite = new SpriteMechanism(gameboard, this, fileName);
 		} catch (IOException e) {
@@ -25,6 +27,10 @@ public class Decoration extends Mechanism{
 	public Decoration(Game frame, Board gameboard, SaveData sd) {
 		super(frame,gameboard,sd);
 		fileName = sd.getString("filename",0);
+		preferredThemes = new String[sd.getData("themes").size()];
+		for(int i=0; i<preferredThemes.length; i++) {
+			preferredThemes[i] = sd.getString("themes",i);
+		}
 		try {
 			sprite = new SpriteMechanism(gameboard, this, fileName);
 		} catch (IOException e) {
@@ -36,7 +42,14 @@ public class Decoration extends Mechanism{
 	public SaveData getSaveData() {
 		SaveData data = new SaveData();
 		data.addData("filename",fileName);
+		for(int i=0; i<preferredThemes.length; i++) {
+			data.addData("themes",preferredThemes[i],i);
+		}
 		return data;
+	}
+	
+	public void randomize(SaveData rnd) {
+		super.randomize(rnd);
 	}
 	
 	public void paint(Graphics g) {
