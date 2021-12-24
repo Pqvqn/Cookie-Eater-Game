e6fe94387a13052905523da5cbf268bdd3ed78f7
Pqvqn <pavanarauch@hotmail.com>
2021-12-23 22:17:44 -0500

    random bushes as well

--- Cookie Eater/src/mechanisms/Decoration.java
+++ Cookie Eater/src/mechanisms/Decoration.java
@@ -49,19 +49,25 @@ public class Decoration extends Mechanism{
 	public void randomize(Level lvl, SaveData rnd) {
 		super.randomize(lvl,rnd);
 		if(!rnd.getBoolean("topassage",0))return;
-		ArrayList<Passage> options = lvl.getPassages();
-		double bestW = 0;
-		Passage best = null;
-		for(Passage p : options) {
-			double currW = preferredThemes.affinityWith(p.getOtherSide().getThemeWeights());
-			if(currW > bestW) {
-				bestW = currW;
-				best = p;
+		boolean goToPassage = Math.random() > rnd.getDouble("passchance",0);
+		if(goToPassage) {
+			ArrayList<Passage> options = lvl.getPassages();
+			double bestW = 0;
+			Passage best = null;
+			for(Passage p : options) {
+				double currW = preferredThemes.affinityWith(p.getOtherSide().getThemeWeights());
+				if(currW > bestW) {
+					bestW = currW;
+					best = p;
+				}
 			}
+			
+			x = board.x_resol/4 + best.getX()/2;
+			y = board.y_resol/4 + best.getY()/2;
+		}else {
+			x = Math.random() * board.x_resol;
+			y = Math.random() * board.y_resol;
 		}
-		
-		x = board.x_resol/4 + best.getX()/2;
-		y = board.y_resol/4 + best.getY()/2;
 		
 	}
 	
