ce769548d1a43f7d42ffa28e9c3bcfd2c6590f7f
Pqvqn <pavanarauch@hotmail.com>
2021-12-29 22:00:13 -0500

    beginning SpriteCombo to reduce drawing time

---
+++ Cookie Eater/src/sprites/SpriteCombo.java
@@ -0,0 +1,18 @@
+package sprites;
+
+import java.util.*;
+import java.awt.*;
+
+import ce3.*;
+
+public class SpriteCombo extends Sprite {
+
+	public SpriteCombo(Board frame, ArrayList<Sprite> subsprites){
+		super(frame);
+	}
+	
+	public void paint(Graphics g) {
+		
+	}
+	
+}
