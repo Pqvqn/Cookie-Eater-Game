0a9d4dfa2c6f04b0381180def924002bae4e868e
Pqvqn <pavanarauch@hotmail.com>
2022-02-08 21:32:10 -0500

    making screen click for drawing Walls

---
+++ Cookie Eater/src/menus/PointClicker.java
@@ -0,0 +1,54 @@
+package menus;
+
+import java.awt.event.*;;
+
+public class PointClicker implements MouseListener, MouseMotionListener{
+
+	public PointClicker() {
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println(e.getX() + ","+ e.getY());
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
